93a18e2957a670fd9b9acc75b8564c3e6e3752b3
mohamednaji7 <n4jidx@gmail.com>
2025-01-17 01:17:33 +0200

    chaning the terminal prompt

---
+++ terminal prompt.sh
@@ -0,0 +1,3 @@
+export PS1="\[\e[1;32m\](\$(basename \$CONDA_DEFAULT_ENV)) \[\e[1;34m\]\u@\h:\[\e[1;33m\]\w\n\[\e[0m\]\$ "
+export PS1="\[\e[1;32m\](unsloth_env) \[\e[1;34m\]\u@\h:\[\e[1;33m\]\w\n\[\e[0m\]\$ "
+export PS1="\[\e[1;32m\](unsloth_env) \[\e[1;34m\]\u@\h:\[\e[1;33m\]\w\[\e[0m\]\$ "
