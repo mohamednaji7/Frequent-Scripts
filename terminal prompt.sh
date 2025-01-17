42520dde32eb65a435a5a301c0d48047a992d04a
mohamednaji7 <n4jidx@gmail.com>
2025-01-17 23:22:31 +0200

    making newline

--- terminal prompt.sh
+++ terminal prompt.sh
@@ -1,3 +1,5 @@
+echo "export PS1='$(if [ -n "$VIRTUAL_ENV" ]; then echo "\[\033[01;35m\]($(basename $VIRTUAL_ENV)) "; fi)\[\033[01;31m\]\u\[\033[01;33m\]@\[\033[01;34m\]\h:\[\033[01;36m\]\w\n\[\033[0m\]$ '" >> ~/.bashrc
+
 echo "export PS1='$(if [ -n "$VIRTUAL_ENV" ]; then echo "\[\033[01;35m\]($(basename $VIRTUAL_ENV)) "; fi)\[\033[01;31m\]\u\[\033[01;33m\]@\[\033[01;34m\]\h:\[\033[01;36m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
 
 echo 'export PS1="\[\e[1;32m\](\$(basename \$CONDA_DEFAULT_ENV)) \[\e[1;34m\]\u@\h:\[\e[1;33m\]\w\n\[\e[0m\]\$ "' >> ~/.bashrc
@@ -6,3 +8,6 @@ export PS1="\[\e[1;32m\](\$(basename \$CONDA_DEFAULT_ENV)) \[\e[1;34m\]\u@\h:\[\
 export PS1="\[\e[1;32m\](unsloth_env) \[\e[1;34m\]\u@\h:\[\e[1;33m\]\w\n\[\e[0m\]\$ "
 
 export PS1="\[\e[1;32m\](unsloth_env) \[\e[1;34m\]\u@\h:\[\e[1;33m\]\w\[\e[0m\]\$ "
+
+source ~/.bashrc
+conda activate unsloth_env
